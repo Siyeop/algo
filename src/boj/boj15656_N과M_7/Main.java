e5958a164bc1e579785d12122172f79632a9bda3
Siyeop <yeop22@naver.com>
2021-02-07 14:40:52 +0900

    BOJ 15656 _ N 과 M (7)
    - 시간복잡도: O(N!)
    - 알고리즘: Backtracking
    - 비고: 15955 문제에 중복가능 조건만 추가

---
+++ src/boj/boj15656_N과M_7/Main.java
@@ -0,0 +1,61 @@
+package boj.boj15656_N과M_7;
+
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    static int M; // 사용가능한 숫자
+    static int N; // 순열 길이
+    static Deque<Integer> usedDeque;
+    static List<Integer> numList;
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream(URLDecoder.decode(Main.class.getResource("").getPath(), "UTF-8") + "sample_input.txt"));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        usedDeque = new ArrayDeque<>();
+
+        st = new StringTokenizer(br.readLine());
+        numList = new ArrayList<>();
+        for(int i=1; i<=M; i++) numList.add(Integer.parseInt(st.nextToken()));
+        Collections.sort(numList);
+
+        for(int i=0; i<M; i++) backtracking(i);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void backtracking(int idx) throws Exception {
+        usedDeque.addLast(numList.get(idx));
+
+        if(usedDeque.size() == N) {
+            printDeque();
+            usedDeque.removeLast();
+            return;
+        }
+
+        for(int i=0; i<M; i++) backtracking(i);
+        usedDeque.removeLast();
+    }
+
+    static void printDeque() throws Exception {
+        Iterator<Integer> iter = usedDeque.iterator();
+        StringBuilder sb = new StringBuilder();
+        while(iter.hasNext()) sb.append(iter.next()).append(" ");
+
+        bw.write(sb.toString().trim() + "\n");
+    }
+}
+
