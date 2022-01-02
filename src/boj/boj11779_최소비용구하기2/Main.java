e3cf54018e7748fb671641c881389e1552c36dae
Siyeop <yeop22@naver.com>
2022-01-02 23:13:15 +0900

    BOJ 11779 _ 최소비용 구하기 2
    - 알고리즘: Dijkstra

--- src/boj/boj11779_최소비용구하기2/Main.java
+++ src/boj/boj11779_최소비용구하기2/Main.java
@@ -3,16 +3,12 @@ package boj.boj11779_최소비용구하기2;
 import java.io.*;
 import java.net.URLDecoder;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     static BufferedReader br;
     static BufferedWriter bw;
     static StringTokenizer st;
-
-    static int N;
-    static int M;
-    static int S;
-    static int E;
 
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream(URLDecoder.decode(Main.class.getResource("").getPath(), "UTF-8") + "sample_input.txt"));
@@ -20,30 +16,31 @@ public class Main {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // 1. 문제 읽기
-        // 1-1. 기본 정보
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        // 1-1. N, M
+        int N = Integer.parseInt(br.readLine().trim()); // 1 ~ 1,000
+        int M = Integer.parseInt(br.readLine().trim()); // 1 ~ 100,000
+
         Graph graph = new Graph(N);
 
-        // 1-2. 간선 정보
+        // 1-2. 연결정보
         for(int i=1; i<=M; i++) {
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine().trim());
             graph.addEdge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        // 1-3. 출발, 도착점
-        st = new StringTokenizer(br.readLine());
-        S = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+        // 1-3. 출발, 도착점 정보
+        st = new StringTokenizer(br.readLine().trim());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
 
-        // 2. 풀이
-        graph.dijkstra(S);
-        graph.setAnswer(S, E);
+        // 2. 계산
+        graph.dijkstra(start);
+        graph.calcShortestRoute(start, end);
 
         // 3. 출력
-        bw.write(graph.cost[E] + "\n");
-        bw.write( graph.pathNodes + "\n");
-        bw.write( graph.path + "\n");
+        bw.write(graph.cost[end] + "\n");
+        bw.write(graph.route.size() + "\n");
+        bw.write(graph.route.stream().map(String::valueOf).collect(Collectors.joining(" ")) + "\n");
 
         bw.flush();
         bw.close();
@@ -53,80 +50,70 @@ public class Main {
 
 class Graph {
     int size;
-    List<Edge>[] adjList;
     int[] cost;
     int[] prev;
-    int pathNodes;
-    String path;
+    List<Node>[] edgeList;
+    boolean[] visited;
+    List<Integer> route;
 
     public Graph(int size) {
         this.size = size;
-        adjList = new ArrayList[size+1];
-        for(int i=1; i<=size; i++) adjList[i] = new ArrayList<>();
         this.cost = new int[size+1];
         this.prev = new int[size+1];
+        this.edgeList = new ArrayList[size+1];
+        for(int i=0; i<=size; i++) this.edgeList[i] = new ArrayList<>();
+        this.visited = new boolean[size+1];
+        this.route = new ArrayList<>();
     }
 
     public void addEdge(int from, int to, int cost) {
-        adjList[from].add(new Edge(to, cost));
+        this.edgeList[from].add(new Node(to, cost));
     }
 
     public void dijkstra(int start) {
-        Arrays.fill(cost, Integer.MAX_VALUE);
-        Arrays.fill(prev, -1);
-        cost[start] = 0;
 
-        Queue<Node> pq = new PriorityQueue<>();
+        Arrays.fill(visited, false);
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        this.cost[start] = 0;
+
+        Queue<Node> pq = new PriorityQueue<>((n1, n2) -> n1.cost - n2.cost);
         pq.add(new Node(start, 0));
 
         while(!pq.isEmpty()) {
             Node polled = pq.poll();
-            for(Edge edge : adjList[polled.num]) {
-                if(polled.cost + edge.cost < cost[edge.to]) {
-                    cost[edge.to] = polled.cost + edge.cost;
-                    prev[edge.to] = polled.num;
-                    pq.add(new Node(edge.to, cost[edge.to]));
+
+            if(!this.visited[polled.to]) {
+                this.visited[polled.to] = true;
+                for(Node edge : this.edgeList[polled.to]) {
+                    int nCost = polled.cost + edge.cost;
+                    if(this.cost[edge.to] > nCost && !this.visited[edge.to]) {
+                        this.cost[edge.to] = nCost;
+                        pq.add(new Node(edge.to, nCost));
+                        this.prev[edge.to] = polled.to;
+                    }
                 }
             }
         }
     }
 
-    public void setAnswer(int start, int end) {
-        Deque<Integer> deque = new ArrayDeque<>();
-        int curr = end;
-        while(prev[curr] != -1) {
-            deque.addFirst(curr);
-            curr = prev[curr];
+    public void calcShortestRoute(int start, int end) {
+
+        int prev = end;
+        this.route.add(prev);
+        while(prev != start) {
+            prev = this.prev[prev];
+            this.route.add(prev);
         }
-        deque.addFirst(start);
-        this.pathNodes = deque.size();
 
-        StringBuilder sb = new StringBuilder();
-        while(!deque.isEmpty()) sb.append(deque.pollFirst()).append(" ");
-        this.path = sb.toString().trim();
+        Collections.reverse(this.route);
     }
 }
 
-class Node implements Comparable<Node> {
-    int num;
-    int cost;
-
-    @Override
-    public int compareTo(Node o) {
-        return this.cost - o.cost;
-    }
-
-    public Node(int num, int cost) {
-        this.num = num;
-        this.cost = cost;
-    }
-}
-
-class Edge {
+class Node {
     int to;
     int cost;
 
-    public Edge(int to, int cost) {
+    public Node(int to, int cost) {
         this.to = to;
         this.cost = cost;
     }
